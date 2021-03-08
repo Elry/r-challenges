1ce4b4aa38a7d27b2d1aa5af32432000d86e8c8e
Elry <rafaelgarcialima@outlook.com>
2021-03-07 23:18:29 -0300

    finished t1, started t2 and add needed data

--- t4-1.r
+++ t4-1.r
@@ -6,6 +6,14 @@ x <- c(3.5, 3.7, 3.8, 4.2, 4.6, 4.7, 4.9, 5.2, 5.5);
 
 df <- data.frame(tempplant = x, waste=y);
 
-g <- ggplot(df, aes(x=tempplant, y=waste)) + geom_point() + labs(x="Time from plantation", y="Waste") + theme_bw();
+# a. gráfico de dispersão
+disp <- ggplot(df, aes(x=tempplant, y=waste)) + geom_point() + labs(x="Time from plantation", y="Waste") + theme_bw();
+
+# b. modelo de regressão linear
 mod <- lm(waste ~ tempplant, data = df);
-g + geom_abline(aes(intercept=coef(mod)[1], slope=coef(mod)[2]))
+
+# c. análise do modelo
+summary(mod);
+
+# d. reta de regressão
+dispLine <- disp + geom_abline(aes(intercept=coef(mod)[1], slope=coef(mod)[2]));
