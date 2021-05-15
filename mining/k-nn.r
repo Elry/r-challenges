728f952aabeff59c8eddd515102045aec932603e
Elry <rafelgarcialima@outlook.com>
2021-05-14 23:15:36 -0300

    Added a better logic to calc without columns that have NA and row NA cleaning

--- mining/k-nn.r
+++ mining/k-nn.r
@@ -10,35 +10,48 @@ summary(biopsy);
 # removing unused column
 biopsy$ID <- NULL;
 
-# cleaning NA's from V6 column
-biopsy <- na.omit(biopsy);
-
-# data analysis without NA's and the ID column
-summary(biopsy);
-
 # function to calc knn
-biopsy_knn <- function(data, k){
+# type === TRUE: row cleaning
+# type === FALSE: column removal
+biopsy_knn <- function(data, k, type){
+  if(type){
+    # cleaning rows from NA values
+    data <- na.omit(data);
+    typeTxt <- "with row cleaning";
+  }else{
+    # removing column with NA
+    data$V6 <- NULL;
+    typeTxt <- "without columns that have NA";
+  }
+  
+  len <- length(data);
+
   # creating accuracy vector
   accuracy = c(1:100)*0
 
   for (i in 1:100) {
     L <- sample(1:nrow(data),round(nrow(data)/3));
     
-    train = data[-L, 1:9];
-    test = data[L, 1:9];
+    train = data[-L, 1:len-1];
+    test = data[L, 1:len-1];
   
-    cl = factor(data[-L, 10]);
+    cl = factor(data[-L, len]);
     fit = knn(train, test, cl, k = k);
   
-    c_matrix = table(fit[1:length(L)], factor(data[L, 10]))
+    c_matrix = table(fit[1:length(L)], factor(data[L, len]))
     
     accuracy[i] = sum(diag(c_matrix))/sum(c_matrix)*100
   }
   
-  cat('\nAccuracy: ', mean(accuracy), '%\n');
+  cat('\nAccuracy', typeTxt, ': ', mean(accuracy), '%\n');
 }
 
-# knn runs
-biopsy_knn(biopsy, 2);
-biopsy_knn(biopsy, 5);
-biopsy_knn(biopsy, 12);
+# knn runs with row cleaning
+biopsy_knn(biopsy, 2, TRUE);
+biopsy_knn(biopsy, 5, TRUE);
+biopsy_knn(biopsy, 12, TRUE);
+
+# nn runs with column cleaning
+biopsy_knn(biopsy, 2, FALSE);
+biopsy_knn(biopsy, 5, FALSE);
+biopsy_knn(biopsy, 12, FALSE);
